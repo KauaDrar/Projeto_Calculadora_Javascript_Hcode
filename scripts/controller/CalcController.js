3130ad9a65994b0627115c8bb603e86aaf98402f
KauaDrar <kauadosreisakamine@hotmail.com>
2023-02-18 12:17:59 -0300

    adding methods to allow the user copy from the calculator or paste a value on the calculator.

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -14,6 +14,30 @@ class CalcController {
     this.initKeyboard();
   }
 
+  pasteFromClipboard() {
+    document.addEventListener("paste", (e) => {
+      let text = e.clipboardData.getData("Text");
+
+      this.displayCalc = parseFloat(text);
+
+      console.log(text);
+    });
+  }
+
+  copyToClipboard() {
+    let input = document.createElement("input");
+
+    input.value = this.displayCalc;
+
+    document.body.appendChild(input);
+
+    input.select();
+
+    document.execCommand("Copy");
+
+    input.remove();
+  }
+
   initialize() {
     this.setDisplayDateTime();
     setInterval(() => {
@@ -21,6 +45,7 @@ class CalcController {
     }, 1000);
 
     this.setLastNumberToDisplay();
+    this.pasteFromClipboard();
   }
 
   initKeyboard() {
@@ -64,6 +89,9 @@ class CalcController {
         case "8":
         case "9":
           this.addOperation(parseInt(e.key));
+          break;
+        case "c":
+          if (e.ctrlKey) this.copyToClipboard();
           break;
       }
     });
