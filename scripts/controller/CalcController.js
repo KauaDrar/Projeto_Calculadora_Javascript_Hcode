fa4335b5034e7985846dfbdad98a76bba7f26917
KauaDrar <kauadosreisakamine@hotmail.com>
2023-02-18 12:12:55 -0300

    created a method to add a dot in order to turn an int number into a float number.

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,6 +2,7 @@ class CalcController {
   constructor() {
     this._lastOperator = "";
     this._lastNumber = "";
+
     this._operation = [];
     this._locale = "pt-BR";
     this._displayCalcEl = document.querySelector("#display");
@@ -28,6 +29,8 @@ class CalcController {
   }
   clearAll() {
     this._operation = [];
+    this._lastNumber = "";
+    this._lastOperator = "";
     this.setLastNumberToDisplay();
   }
 
@@ -128,8 +131,6 @@ class CalcController {
     if (isNaN(this.getLastOperation())) {
       if (this.isOperator(value)) {
         this.setLastOperation(value);
-      } else if (isNaN(value)) {
-        console.log("Outra coisa", value);
       } else {
         this.pushOperation(value);
         this.setLastNumberToDisplay();
@@ -139,7 +140,7 @@ class CalcController {
         this.pushOperation(value);
       } else {
         let newValue = this.getLastOperation().toString() + value.toString();
-        this.setLastOperation(parseInt(newValue));
+        this.setLastOperation(parseFloat(newValue));
 
         this.setLastNumberToDisplay();
       }
@@ -148,6 +149,18 @@ class CalcController {
 
   setError() {
     this.displayCalc = "Error";
+  }
+
+  addDot() {
+    let lastOperation = this.getLastOperation();
+
+    if (this.isOperator(lastOperation) || !lastOperation) {
+      this.pushOperation("0.");
+    } else {
+      this.setLastOperation(lastOperation.toString() + ".");
+    }
+
+    this.setLastNumberToDisplay();
   }
 
   execBtn(value) {
@@ -185,7 +198,7 @@ class CalcController {
         break;
 
       case "ponto":
-        this.addOperation(".");
+        this.addDot();
         break;
 
       case "0":
